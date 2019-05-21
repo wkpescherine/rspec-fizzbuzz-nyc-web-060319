6f2e3446d2af09aa789ba30ef2a8790cda289ad4
William Pescherine <wkpescherine@gmail.com>
2019-05-21 04:22:51 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
-  if num/3
+  if num%3 == 0
     print "Fizz"
   elsif num%5 == 0
     print "Buzz"
